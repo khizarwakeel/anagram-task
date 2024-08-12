a42a863c915a51983256c9c5f0de4aedede1bd79
Khizar Wakeel <khizarwakeel@gmail.com>
2024-08-13 02:20:16 +0500

    added logic of anagram

---
+++ App.js
@@ -0,0 +1,23 @@
+
+// Anagram Detector App
+
+function anagramsFunc(arrayOfWords) {
+    let anagrams = {};
+    for (let word of arrayOfWords) {
+        let sortedWords = word.split('').sort().join('');
+
+        if (!anagrams[sortedWords]) {
+            anagrams[sortedWords] = [];
+        }
+
+        anagrams[sortedWords].push(word);
+    }
+
+    console.log(anagrams);
+    return Object.values(anagrams);
+}
+
+let arrayOfWords = ["bat", "tap", "cat", "tab", "fats", "pat", "khizar", "fast"];
+let newAnagramArray = anagramsFunc(arrayOfWords);
+
+console.log(newAnagramArray);
